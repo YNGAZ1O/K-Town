1cd08304400d01050becfda1970396ee20d454dd
YNGAZ1O <alotaibe1asy@gmail.com>
2026-04-10 16:11:26 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,8 +25,8 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
-        // Activity type number (0 = Playing).
+        name: "In K-Town",
+        // Activity type number (4 = cruising).
         type: 0, 
       },
     ],
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#baac63", 
+      secondary: "#b3edff", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
